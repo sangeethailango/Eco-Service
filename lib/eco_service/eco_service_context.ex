c696681c06982f65625a7159351d200b82e453a6
sangeetha <sangeethailango21@gmail.com>
2024-03-15 15:13:48 +0530

    Pagination for community list page

--- lib/eco_service/eco_service_context.ex
+++ lib/eco_service/eco_service_context.ex
@@ -1,9 +1,18 @@
 defmodule EcoService.EcoServiceContext do
 
+  import Ecto.Query
   alias EcoService.Repo
   alias EcoService.EcoService.Community
 
+  def fetch_all_communities(params) do
+    Community
+    |> limit(^params.limit)
+    |> offset(^params.offset)
+    |> Repo.all()
+  end
+
   def fetch_all_communities() do
-    Repo.all(Community)
+    Community
+    |> Repo.all()
   end
 end
